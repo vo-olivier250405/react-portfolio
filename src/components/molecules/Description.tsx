984db1d440379ecc4695a00803dde7ce5d8c9856
vo_o <vo_o@etna-alternance.net>
2024-01-02 12:04:28 +0100

    [ADD] => add new home page design

--- src/components/molecules/Description.tsx
+++ src/components/molecules/Description.tsx
@@ -6,8 +6,14 @@ export const Description = () => {
       <img src="src/assets/pic1.jpg" alt="profile-picture" />
       <BubbleBox
         text={
-          <h1>
-            Hi, im <span>Olivier</span>, Etna student !
+          <h1 className="text-h1">
+            Hi, Im <span className="text-h1"> Vo Olivier</span>, Etna student !
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+              totam sunt aperiam corrupti provident mollitia architecto. Quia
+              nulla ratione dolor maiores, optio reiciendis nesciunt error
+              veritatis, minus non recusandae debitis.
+            </p>
           </h1>
         }
       />
