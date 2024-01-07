04417e20635c17023236c864c421d0a833191bd0
vo_o <vo_o@etna-alternance.net>
2024-01-07 11:10:44 +0100

    [ADD => adding projects links

--- src/components/molecules/Projects.tsx
+++ src/components/molecules/Projects.tsx
@@ -1,7 +1,30 @@
 export const Projects = () => {
   return (
-    <div>
-      <h1>HDZLIJ</h1>
+    <div className="projects">
+      <div className="projects-child">
+        <a
+          href="https://github.com/vo-olivier250405/pythonFinancialDashboard"
+          target="blank"
+        >
+          Python Financial Dashboard
+        </a>
+      </div>
+      <div className="projects-child">
+        <a
+          href="https://github.com/vo-olivier250405/pythonFinancialDashboard"
+          target="blank"
+        >
+          Gamify Your tasks | Web Application
+        </a>
+      </div>
+      <div className="projects-child">
+        <a
+          href="https://drive.google.com/file/d/1pCu_qf83LbPwMSwA57ww89hpfxn9TyKJ/view"
+          target="blank"
+        >
+          Python Fighter Game
+        </a>
+      </div>
     </div>
   );
 };
